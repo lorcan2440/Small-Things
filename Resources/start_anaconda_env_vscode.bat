7336e743b563a7771675465024ea4b3f93100fe4
Lorcan Nicholls <72615977+lorcan2440@users.noreply.github.com>
2021-12-01 12:07:21 +0000

    Add files via upload

---
+++ Resources/start_anaconda_env_vscode.bat
@@ -0,0 +1,4 @@
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe                                                               ^
+    -ExecutionPolicy ByPass                                                                                           ^
+    -NoExit                                                                                                           ^
+    -Command "& 'C:\Users\lnick\anaconda3\shell\condabin\conda-hook.ps1'; conda activate 'C:\Users\lnick\anaconda3'; conda activate AllLibs39; code"
---
+++ Resources/start_jupyter_notebook.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set ANACONDAPATH=C:\Users\lnick\anaconda3
+
+%ANACONDAPATH%\python.exe %ANACONDAPATH%\cwp.py %ANACONDAPATH% ^
+    %ANACONDAPATH%\python.exe %ANACONDAPATH%\Scripts\jupyter-notebook-script.py %1
